496040bd459f66ec75753111e9af4abce0bfe106
Aiswaryasenthilkumar <ishusenthil2804@gmail.com>
2021-03-28 16:01:03 +0530

    commited all files

---
+++ salaryproj/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.staticfiles.storage import staticfiles_storage
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+def home(request):
+    return render(request,'index.html',{"predicted":""})
+
+
+
+def predict(request):
+    exp = float(request.GET['exp'])
+    rawdata = staticfiles_storage.path('Salary_Data.csv')
+    dataset = pd.read_csv(rawdata)
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, 1].values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.1, random_state=0)
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+    yet_to_predict = np.array([[exp]])
+    y_pred = regressor.predict(yet_to_predict)
+    accuracy = regressor.score(X_test, y_test)
+    accuracy = accuracy*100
+    accuracy = int(accuracy)
+    return render(request,'index.html',{"predicted":int(y_pred[0]),"exp":exp})
+
+
+
+
+def courses(request):
+    data = "this is courses page"
+    return HttpResponse(data)
